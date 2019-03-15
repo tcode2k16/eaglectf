e2a99cca7a3314d5513e6ad62be3eaa37d43416a
tcode2k16 <tcode2k16@gmail.com>
2019-03-15 08:19:04 +0800

    add feature: get all url

--- commands/geturl.js
+++ commands/geturl.js
@@ -1,6 +1,8 @@
 const { oneLine } = require('common-tags');
 const Commando = require('discord.js-commando');
 
+const { table } = require('table');
+
 const { db, getUser } = require('../db');
 
 const game = require('../game.json');
@@ -18,12 +20,19 @@ module.exports = class GeturlCommand extends Command {
 			throttling: {
 				usages: 5,
 				duration: 10
-			}
+			},
+			args: [{
+        key: 'isAll',
+        label: 'getAllUrl',
+        prompt: 'Do you want to see the urls for all the past levels? Do "geturl all".',
+        type: 'string',
+        default: '',
+      }]
 		});
 	}
 
 
-	run(msg) {
+	run(msg, { isAll }) {
 		if (msg.channel.type !== 'dm') {
       msg.author.createDM().then(c => c.send('You can only use this command in direct message'));
       msg.delete();
@@ -31,6 +40,19 @@ module.exports = class GeturlCommand extends Command {
 		}
 		
 		const user = getUser(msg.author);
+
+		if (isAll === 'all') {
+			let data = [['Level', 'URL']];
+			let upperLimit = this.client.isOwner(msg.author) ? game.maxLevel-1 : user.level;
+			upperLimit = Math.min(upperLimit, game.maxLevel-1);
+			for (let i = 0; i <= upperLimit; i++) {
+				data.push([`level ${i}`, game.urls[i]]);
+			}
+			return msg.reply(table(data), {
+				split: true,
+				code: true,
+			});
+		}
 
 		if (user.level >= game.maxLevel) return msg.reply('Good Job! You finished all the challenges.');
 		
